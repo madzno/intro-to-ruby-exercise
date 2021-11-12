4919b659ad6a6f686a9280aea791879ee6f43ce3
MadzNo <madzno12@gmail.com>
2021-11-12 18:02:39 +0000

    adding chapter 2 exercises

---
+++ chapter_2/age.rb
@@ -0,0 +1,12 @@
+puts "How old are you?"
+age = gets.chomp
+
+age_ten = (age.to_i) + 10
+age_20 = (age.to_i) + 20
+age_30 = (age.to_i) + 30
+age_40 = (age.to_i) + 40
+
+puts "In 10 years you will be #{age_ten} !" 
+puts "In 20 years you will be #{age_20} !"
+puts "In 30 years you will be #{age_30} !"
+puts "In 40 years you will be #{age_40} !"
---
+++ chapter_2/exercise2_5.rb
@@ -0,0 +1,3 @@
+# The first X gives out the output "3" 
+# The second X example gives an error message because the "x" variable is only defined inside the scope of the block and it is being called
+# outside of the block in the example.
