f4b0f987e79deb85b26f70f5e25791a87347c770
MadzNo <madzno12@gmail.com>
2021-11-13 18:24:50 +0000

    adding chapter 3 exercises

---
+++ chapter_3/exercise3_4.rb
@@ -0,0 +1,9 @@
+def scream(words)
+    words = words + "!!!!"
+    puts words
+end 
+
+scream("Yippeee")
+
+#This code puts "Yippeee!!!!" to the screen but returns "nil" because "puts" statements output to the screen but do not return their value
+# thus always returning "nil"
---
+++ chapter_3/exercise3_6.rb
@@ -0,0 +1,2 @@
+# This error message lets you know that you invocated a method named calculate_product that takes two arguments (a,b) however you only passed
+# the method one argument (a)
