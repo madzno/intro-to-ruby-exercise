a92c6275d593aafebc29a8611bbf55eab3052930
MadzNo <madzno12@gmail.com>
2021-11-17 22:19:59 +0000

    adding final exercises

---
+++ final_exercises/exercise_3.rb
@@ -0,0 +1,4 @@
+array = [5, 6, 7, 8, 9, 10]
+
+#3
+new_array = array.select { |num| num % 2 != 0}
---
+++ final_exercises/exercise_4.rb
@@ -0,0 +1,18 @@
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+#4a
+array.push(11)
+
+#4b
+array.unshift(0)
+
+#5a
+array.delete(11)
+
+#or array.pop 
+
+#5b
+array.push(3)
+
+#6
+array.uniq
